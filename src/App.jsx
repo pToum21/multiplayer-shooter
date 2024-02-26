ced00902c59f2a1bb989ec2922e1d167a730e1ff
toum21 <pey2moo1@gmail.com>
2024-02-26 11:04:42 -0800

    updated shaders on the map to show shadows and blend them nicley as well as installed playroomkit using yarn

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { SoftShadows } from "@react-three/drei";
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 30, 0], fov: 30 }}>
       <color attach="background" args={["#242424"]} />
+      <SoftShadows size={42} />
       <Experience />
     </Canvas>
   );
