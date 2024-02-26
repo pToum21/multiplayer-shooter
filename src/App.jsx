98cefd25726a6ed6c812e54882a1e83a1ffb9ce5
toum21 <pey2moo1@gmail.com>
2024-02-26 11:50:59 -0800

    using the joystick you can now move the character but it is to fast and he also will fall over

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { SoftShadows } from "@react-three/drei";
+import { Suspense } from "react";
+import { Physics } from "@react-three/rapier";
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 30, 0], fov: 30 }}>
+
       <color attach="background" args={["#242424"]} />
+
       <SoftShadows size={42} />
-      <Experience />
+
+      <Suspense>
+
+        <Physics debug>
+          <Experience />
+        </Physics>
+
+      </Suspense>
+
     </Canvas>
   );
 }
--- src/components/CharacterController.jsx
+++ src/components/CharacterController.jsx
@@ -1,5 +1,11 @@
 import { useRef, useState } from "react"
 import { CharacterSoldier } from "./CharacterSoldier";
+import { CapsuleCollider, RigidBody } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+
+
+const MOVEMENT_SPEED = 200
+
 
 export const CharacterContoroller = (
     { state, joystick, userPlayer, ...props }
@@ -7,14 +13,38 @@ export const CharacterContoroller = (
     const group = useRef();
     const character = useRef();
     const [animation, setAnimation] = useState('Idle');
+    const rigidbody = useRef();
+
+    useFrame((_, delta) => {
+        const angle = joystick.angle();
+        if (joystick.isJoystickPressed() && angle) {
+            setAnimation('Run');
+            character.current.rotation.y = angle;
+
+            const impulse = {
+                x: Math.sin(angle) * MOVEMENT_SPEED * delta,
+                y: 0,
+                z: Math.cos(angle) * MOVEMENT_SPEED * delta,
+            };
+
+            rigidbody.current.applyImpulse(impulse, true);
+        } else {
+            setAnimation('Idle');
+        }
+    })
+
+
     return (
         <group ref={group} {...props}>
-            <group ref={character}>
-                <CharacterSoldier
-                    color={state.state.profile?.color}
-                    animation={animation}
-                />
-            </group>
+            <RigidBody ref={rigidbody} colliders={false}>
+                <group ref={character}>
+                    <CharacterSoldier
+                        color={state.state.profile?.color}
+                        animation={animation}
+                    />
+                </group>
+                <CapsuleCollider args={[0.7, 0.6]} position={[0, 1.28, 0]} />
+            </RigidBody>
         </group>
     )
 }
