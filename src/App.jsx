eca652195661ebcdf4621712d7f3c3c47e5f3c2a
toum21 <pey2moo1@gmail.com>
2024-02-27 18:52:29 -0800

    updated the bullet to look more like a lazer and added audio files to the files but not into code yet

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,21 @@ import { Experience } from "./components/Experience";
 import { SoftShadows } from "@react-three/drei";
 import { Suspense } from "react";
 import { Physics } from "@react-three/rapier";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 30, 0], fov: 30 }}>
-
+    <Canvas shadows camera={{ position: [0, 30, 0], fov: 30, near: 2 }}>
       <color attach="background" args={["#242424"]} />
-
       <SoftShadows size={42} />
-
       <Suspense>
-
         <Physics>
           <Experience />
         </Physics>
-
       </Suspense>
-
+      <EffectComposer disableNormalPass>
+        <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
+      </EffectComposer>
     </Canvas>
   );
 }
