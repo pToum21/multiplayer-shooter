7b6f7cc812b1ebd3906cb37662f6209bbf459258
toum21 <pey2moo1@gmail.com>
2024-02-28 14:22:16 -0800

    created player info variable

--- src/components/CharacterController.jsx
+++ src/components/CharacterController.jsx
@@ -44,9 +44,9 @@ export const CharacterContoroller = (
 
 
     useEffect(() => {
-if (isHost()) {
-    spawnRandomly()
-}
+        if (isHost()) {
+            spawnRandomly()
+        }
     }, [])
 
 
@@ -160,6 +160,30 @@ if (isHost()) {
         </group>
     )
 }
+
+
+
+const PlayerInfo = ({ state }) => {
+    const health = state.health;
+    const name = state.profile.name;
+    return (
+        <Billboard position-y={2.5}>
+            <Text position-y={0.36} fontSize={0.4}>
+                {name}
+                <meshBasicMaterial color={state.profile.color} />
+            </Text>
+            <mesh position-z={-0.1}>
+                <planeGeometry args={[1, 0.2]} />
+                <meshBasicMaterial color="black" transparent opacity={0.5} />
+            </mesh>
+            <mesh scale-x={health / 100} position-x={-0.5 * (1 - health / 100)}>
+                <planeGeometry args={[1, 0.2]} />
+                <meshBasicMaterial color="red" />
+            </mesh>
+        </Billboard>
+    );
+};
+
 
 
 const Crosshair = (props) => {
